4a09c50875900117e52aed2f5b7feff8be9a3703
Dmitriy Gaydukov <gaydukov89@gmail.com>
2024-05-16 17:24:45 +0400

    update nacos to work with spring-boot 3.2

--- src/main/java/com/exchange/asset/config/AppConfig.java
+++ src/main/java/com/exchange/asset/config/AppConfig.java
@@ -15,6 +15,6 @@ public class AppConfig {
   @Value("${app.config.env:dev}")
   private String configEnv;
 
-  @Value("${app.config.print:false}")
+  @Value("${app.config.print:true}")
   private boolean printConfig;
 }

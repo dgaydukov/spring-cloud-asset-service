287a389fc70f4234468bdc99cbbcbb1f0de92392
Dmitriy Gaydukov <gaydukov89@gmail.com>
2024-05-21 12:18:49 +0400

    remove feign config

--- src/main/java/com/exchange/asset/AssetService.java
+++ src/main/java/com/exchange/asset/AssetService.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableFeignClients
 @EnableDiscoveryClient
 public class AssetService {
 
